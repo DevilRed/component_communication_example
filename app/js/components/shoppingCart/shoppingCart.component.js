dffb15e005f88ae2f366a0acbdfa8c357993bd91
Christian <christian.quispe@avantica.net>
2018-09-12 17:36:31 -0400

    check if product is already sold

--- app/js/components/shoppingCart/shoppingCart.component.js
+++ app/js/components/shoppingCart/shoppingCart.component.js
@@ -16,13 +16,39 @@
 			{ name: 'Microwave', price: 150},
 			{ name: 'ChromeCast', price: 70},
 		];
-		ctrl.selectedProduct = [];
+		ctrl.$onInit = function (){
+			ctrl.selectedProduct = {};
+			ctrl.soldProducts = [];
+		};
 
 	  cartService.onItemsAdded(function(item, quantity){
 	    ctrl.selectedProduct = item;
 	    ctrl.selectedProduct.quantity = quantity;
 	    ctrl.selectedProduct.total = item.price * quantity;
-	    console.log(ctrl.selectedProduct);
+
+	    var isSoldProduct = checkIfSoldProduct(ctrl.soldProducts, ctrl.selectedProduct);
+	    console.log(isSoldProduct);
+	    if(ctrl.soldProducts.length == 0 || !isSoldProduct) {
+	    	ctrl.soldProducts.push(ctrl.selectedProduct);
+	    } else {
+	    	ctrl.soldProducts.forEach(function (el){
+	    		if(el.name === ctrl.selectedProduct.name) {
+	    			ctrl.selectedProduct.quantity = quantity;
+	    		}
+	    	});
+	    }
 	  });
+	};
+
+	function checkIfSoldProduct(items, sold){
+		var result = false;
+		if( items.length != 0) {
+		items.forEach(function (el){
+			if( el.name === sold.name ) {
+				result = true;
+			}
+		});
+		}
+		return result;
 	};
 })();
