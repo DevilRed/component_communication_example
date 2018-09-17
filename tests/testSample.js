fbaa92c8a2562650d920cdf1b1cfa879d521565d
Christian <christian.quispe@avantica.net>
2018-09-17 11:53:32 -0400

    testing cartService, check the how the service is injected

--- tests/testSample.js
+++ tests/testSample.js
@@ -1,16 +1,15 @@
-describe('Filters', function(){ //describe your object type
-    beforeEach(module('workers')); //load module
-    describe('reverse',function(){ //describe your app name
-        var reverse;
-        // beforeEach, inject  are jasmine API
-        beforeEach(inject(function($filter){ //initialize your filter
-            reverse = $filter('reverse',{});
+describe('buyItNow a small angularJS app', function(){ //describe your object type
+    beforeEach(module('buyItNow')); //load module
+    describe('cartService is a provider service',function(){ //describe your app name
+        beforeEach(module("buyItNow"));
+        var cartService;
+        beforeEach(inject(function (_cartService_){
+            cartService = _cartService_;
         }));
-        it('Should reverse a string', function(){  //write tests
-            // expect, toBe  are jasmine API also
-            expect(reverse('rahil')).toBe('lihar'); //pass
-            expect(reverse('don')).toBe('nod'); //pass
-            // expect(reverse('jam')).toBe('oops'); // this test should fail
+
+        it('should return products', function (){
+            var products = cartService.getProducts();
+            expect(products).not.toBe(null);
         });
     });
 });
