6521de64c48d0b3c03a3d25d3a60183569de6d40
Christian <christian.quispe@avantica.net>
2018-09-19 11:00:56 -0400

    add gulp-inject and clarify task's name for jslinting

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var watchify    = require('watchify');
 var buffer      = require('vinyl-buffer');
 var gutil       = require('gulp-util');
 var rimraf      =  require('rimraf');
+var inject = require('gulp-inject');
 
 var env = process.env.NODE_ENV || 'development';
 
@@ -29,15 +30,14 @@ var path = {
 
 gulp.task('browser-sync', function (){
     browserSync.init({
-        // server: "app", port: 9000
         server: path.app, port: 9000
     });
 });
-// linting
+// js linting
 /**
  * this task follows the options defined in .jshintrc file, to see more explanation about the options: https://github.com/jshint/jshint/blob/master/examples/.jshintrc
  */
-gulp.task('lint', function (){
+gulp.task('jslint', function (){
     return gulp.src([path.app + 'js/components/**/*.js', path.app + 'js/*.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
@@ -127,28 +127,26 @@ gulp.task('sprite', function (){
     spriteData.css.pipe(gulp.dest(path.app + 'scss'));
 });
 
+// inject,  about paths:   https://github.com/klei/gulp-inject/wiki/
+gulp.task('inject', function (){
+    gulp.src(path.app + 'index.html')
+        .pipe(inject(gulp.src([path.js_dest + path.js_outputFile, path.app + 'css/style.css'], { read: false }), {relative: true}))
+        .pipe(gulp.dest(path.app))
+    ;
+});
+
 // watch for changes
 gulp.task('watch', ['browser-sync'], function (){
     gulp.watch([ path.app + 'js/components/**/*.html' ], ['templates']);
     gulp.watch([path.app + 'scss/*.scss'], ['sass']);
-     gulp.watch([path.app + 'js/**/*.js'], ['bundle']);
+    gulp.watch([path.app + 'js/**/*.js'], ['bundle']);
+    gulp.start('inject');// will be deprecated, though
 });
 
 
 gulp.task('default', ['watch']);
 
 
-/*
-    project structure:
-    - main folder (.bowerrc, .gitignore, bower.json, gulpfile.js, package.json)
-        - app (index.html)
-            - css
-            - js
-                - components (app.module.js, app.routes.js, templates.js)
-                    - requiredFunctionality
-            - scss
-        - node_modules
- */
 /*
  TDD can be described as:
     * write a "single" unit test describing an aspect of the program
@@ -172,83 +170,11 @@ gulp.task('default', ['watch']);
 
 
 KARMA CONFIGURATION
-source: https://code.ciphertrick.com/2015/08/03/angularjs-unit-testing-tutorial-karma-jasmine/
-- run: "karma init" from windows cmd to generate karma.conf.js file
-- run: "karma start" to start testing
-
-
-* Install Karma
-    npm install -g karma --save-dev
-* Install Jasmine
+    npm install -g karma karma-cli --save-dev
     npm install karma-jasmine jasmine-core --save-dev
-* Install ngMock, ngMock allows you to inject and mock angular services to help you test your application.
-    npm install angular-mocks --save-dev
-* Browsers, install browser launcher on which you want karma to run your tests. We need to install at least one browser. I’ll use PhantomJs.
+    npm install angular-mocks --save-dev   # ngMock allows you to inject and mock angular services to help you test your application.
     npm install karma-phantomjs-launcher --save-dev
 
-
-minimal karma.conf.js content example:
-// Karma configuration
-// Generated on Sat Jul 25 2015 19:12:21 GMT+0530 (India Standard Time)
-module.exports = function(config) {
-  config.set({
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-    // list of files / patterns to load in the browser
-    files: [
-      'node_modules/angular/angular.js',
-      'node_modules/angular-mocks/angular-mocks.js',
-      'app/app.js',  //use wildcards in real apps
-      'tests/tests.js' //use wildcards in real apps
-    ],
-    // list of files to exclude
-    exclude: [
-    ],
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-    // web server port
-    port: 9876,
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
-  })
-}
-
-
-
-
-
-TESTING PATTERN
-
-There are a few ways to write your tests to keep it simple and organised, I’ll show you the pattern I follow.
-
-* Describe your object type. eg: Controller, Filters, Factory, etc.
-* Inject/load app module
-* Describe your specific object name.
-* Load mock modules as required
-* Initialize your object.
-* Describe specific method (for controllers and services)
-* Write your tests, expectations.
-
-
-
+    karma init   # to generate karma.conf.js file
+    karma start  # to start testing
 */
